32d1120143db80d19e240063cad2c9bf1568566c
sirojiddin1017 <abduraufovsirojiddin@gmail.com>
2024-12-18 09:40:07 +0500

    Update models.py
    Versiy 1.1

--- home/models.py
+++ home/models.py
@@ -1,7 +1,24 @@
 from django.db import models
 
 # Create your models here.
-class Setting():
+class Setting(models.Model):
+    title = models.CharField(max_length=100)
+    keywords = models.CharField(max_length=100)
+    description = models.TextField(max_length=500)
+    phone = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    adres = models.CharField(max_length=100)
+    smtp_server = models.CharField(max_length=100)
+    smtp_email = models.CharField(max_length=100)
+    smtp_pasword = models.CharField(max_length=100)
+    smtp_port = models.CharField(max_length=100)
+    youtube = models.CharField(max_length=100)
+    instagram = models.CharField(max_length=100)
+    facebook = models.CharField(max_length=100)
+    icone = models.ImageField(null=False, upload_to='images/')
+    aboutus = models.CharField(max_length=100)
+    contact = models.CharField(max_length=100)
+    
     def __str__(self):
         return self.title
 
